323a3a58d4fe3d0c9a3ea0a56627cb7b9d13e0a0
Roofikk <rufik956@gmail.com>
2021-04-08 13:17:56 +0300

    Command init v.2

--- Manager/AvestaClientManager.php
+++ Manager/AvestaClientManager.php
@@ -7,5 +7,12 @@ use roofikk\AvestaBundle\Exception\AvestaException;
 
 class AvestaClientManager
 {
+    public $testVar;
+    public function __construct($testVar){
+        $this->testVar = $testVar;
+    }
 
+    public function testFunc() {
+        var_dump($this->testVar.'from function');
+    }
 }
