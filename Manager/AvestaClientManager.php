c8f0c7634972ebbf97eafe7db31438df4cf7f90b
Roofikk <rufik956@gmail.com>
2021-04-22 13:39:33 +0300

    Test please

---
+++ Manager/AvestaClientManager.php
@@ -0,0 +1,18 @@
+<?php declare(strict_types=1);
+
+namespace roofikk\AvestaBundle\Manager;
+
+use roofikk\AvestaBundle\Contracts\ApiClientInterface;
+use roofikk\AvestaBundle\Exception\AvestaException;
+
+class AvestaClientManager
+{
+//    public $testVar;
+//    public function __construct($testVar){
+//        $this->testVar = $testVar;
+//    }
+//
+//    public function testFunc() {
+//        var_dump($this->testVar.'from function');
+//    }
+}
